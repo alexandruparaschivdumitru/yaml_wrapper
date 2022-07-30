0e3d72b798477a428a537b02df3fd66e09e75aad
Alexandru Dumitru <aaaalexdumitru@gmail.com>
2022-07-30 18:16:28 +0200

    Partial implementation of to_yaml_traslator

--- src/modules/translators/to_yaml_translator.py
+++ src/modules/translators/to_yaml_translator.py
@@ -2,8 +2,11 @@ from io import TextIOWrapper
 from yaml import dump as upload_data # type: ignore
 from yaml import Dumper
 from yaml import YAMLError
+from typing import List, Union
 
 from src.modules.translators.exceptions.writing_yaml_exception import WritingYamlException
+from src.modules.yaml_structures.yaml_dictionary import YamlDictionary
+from src.modules.yaml_structures.yaml_list import YamlList
 
 
 class ToYamlTranslator:
@@ -30,4 +33,30 @@ class ToYamlTranslator:
         except YAMLError:
             raise WritingYamlException("Error writing the content to the yaml file.")
     
+    def _apply_rule_rec(self, objects: List[Union[YamlDictionary, YamlList]], returned_value: dict) -> dict:
+        if len(objects) == 0:
+            return returned_value
+        else:
+            first_element: Union[YamlDictionary, YamlList] = objects[0]
+            if(isinstance(first_element, YamlDictionary)):
+                #TODO: Continue implementing the rule
+                returned_value[first_element.key] = self._define_rule_to_apply(first_element.value)
+         
+        return {} # TODO: remove this return during implementation            
     
+    def _define_rule_to_apply(self, object: Union[int, 
+                                            str, 
+                                            List[YamlDictionary], 
+                                            YamlDictionary, 
+                                            List["YamlList"],  
+                                            YamlList]) -> Union[int,str, dict, list]:
+        
+        # TODO: Implemet all rules of the recusion that are based value.
+        # object is:                    RETURN:
+        # - int                         - int
+        # - str                         - str
+        # - List[YamlDictionary]        - recusion([YamlDictionary], [])
+        # - YamlDictionary              - returned_value.[YamlDictionary.key] =  define_rule(YamlDictionary.value]+)
+        # - List["YamlList"]            - recusion([YamlList], [])
+        # - YamlList                    - if list = generic list return list, else recusion([YamlDictionary] or [YamlList], [])
+        return {}
